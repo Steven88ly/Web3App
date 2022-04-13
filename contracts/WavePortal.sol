7a4f31b1d902dd995e4b4485e043337a4ee323dc
Steven88ly <steven88ly@gmail.com>
2022-04-13 18:51:37 -0400

    Interacting with smart contract
    created a script that deploys my smart contract and interacts with the public functions and playing with addresses and state variables

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -1,10 +1,25 @@
-
+//SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+    mapping(address => uint256) public waveMap;
+
     constructor() {
-        console.log("Yo yo, I am a contract and I am smart");
+        console.log("REEEEEEEEEEEEEEEEE");
+    }
+
+    function wave() public {
+        totalWaves++;
+        waveMap[msg.sender]++;
+        console.log("%s has waved %d times!", msg.sender, waveMap[msg.sender]);
+        
+    }
+
+    function getTotalWaves() public view returns (uint256){
+        console.log("We have %d total waves", totalWaves);
+        return totalWaves;
     }
 }
